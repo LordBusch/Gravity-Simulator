de609d3e415236393f900fcb27a5136d4098f3e5
Henry <henry_buschmann@web.de>
2023-06-12 22:25:22 +0200

    Body classes

--- src/bodies/MainBody.java
+++ src/bodies/MainBody.java
@@ -1,6 +1,9 @@
 package bodies;
+
+import java.awt.Color;
+
 public class MainBody {
-    public MainBody(int x) {
+    public MainBody(int mass, int radius, int x, int y, Color color) {
 
     }
 }
---
+++ src/bodies/SecondaryBody.java
@@ -0,0 +1,9 @@
+package bodies;
+
+import java.awt.Color;
+
+public class SecondaryBody {
+    public SecondaryBody(int velocity, int mass, int radius, int x, int y, Color color) {
+
+    }
+}
