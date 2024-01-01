5546c5c6c2ddb77003bd22df8e77d6b42f197210
LordBusch <87705117+LordBusch@users.noreply.github.com>
2024-01-01 19:28:00 +0100

    trace UI
    - UI components for trace (not implemented yet)

--- src/framecomponents/panels/GeneralPanel.java
+++ src/framecomponents/panels/GeneralPanel.java
@@ -3,6 +3,7 @@ package framecomponents.panels;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
 import javax.swing.*;
 
@@ -12,13 +13,15 @@ public class GeneralPanel extends JPanel implements ActionListener {
     JButton LoadButton = new JButton("Load");
     JButton SaveButton = new JButton("Save");
     JButton ResetButton = new JButton("Reset");
+    JCheckBox TraceCheckBox = new JCheckBox("Show trace");
+    JSlider TraceSlider = new JSlider();
 
     GridBagLayout layout = new GridBagLayout();
     GridBagConstraints gbc = new GridBagConstraints();
 
     GeneralPanel() {
         initialize();
-        setUpButtons();
+        setUpComponents();
     }
 
     private void initialize() {
@@ -39,13 +42,43 @@ public class GeneralPanel extends JPanel implements ActionListener {
         gbc.gridy = 1;
         gbc.gridwidth = 3;
         this.add(PauseButton, gbc);
+        /*
+        gbc.gridy = 2;
+        this.add(TraceCheckBox, gbc);
+        gbc.gridy = 3;
+        this.add(TraceSlider, gbc);
+        */
     }
 
-    private void setUpButtons() {
+    private void setUpComponents() {
+        // Buttons
         PauseButton.addActionListener(this);
         LoadButton.addActionListener(this);
         SaveButton.addActionListener(this);
         ResetButton.addActionListener(this);
+
+        /*
+        // Checkbox
+        TraceCheckBox.setBackground(getBackground());
+
+        // Slider
+        Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+        labelTable.put(0, new JLabel("0") );
+        labelTable.put(10, new JLabel("10") );
+        labelTable.put(20, new JLabel("20") );
+        labelTable.put(30, new JLabel("30") );
+        labelTable.put(40, new JLabel("40") );
+        labelTable.put(50, new JLabel("Ꝏ") );
+        TraceSlider.setLabelTable(labelTable);
+
+        TraceSlider.setMinimum(0);
+        TraceSlider.setMaximum(50);
+		TraceSlider.setMajorTickSpacing(10);
+		TraceSlider.setMinorTickSpacing(2);
+		TraceSlider.setPaintTicks(true);
+		TraceSlider.setPaintLabels(true);
+        TraceSlider.setBackground(getBackground());
+        */
     }
 
     public void actionPerformed(ActionEvent e) {
